6cd8257e2ca673421f232dd549ddfbe8ed59f4e7
msa0683 <msa0683@yahoo.com>
2017-07-24 07:09:21 -0700

    homepage html done

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser= require("body-parser");
+var PORT = process.env.PORT || 8080;
+
+app.get("/", function(req, res) {
+	res.send("Hello world");
+});
+
+
+app.use(bodyParser.json())
+
+app.use(function(req, res) {
+	res.setHeader('Content-Type', 'text/plain')
+	res.write('you posted\n')
+	res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.listen(PORT, function(){
+	console.log("APP listening on Port: " + PORT);
+});
+
+
+
+
+
+
+
+
+
+
+
