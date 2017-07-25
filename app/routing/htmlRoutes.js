fd101fb7e1497d1bdb656e5ece2ed15346256e51
msa0683 <msa0683@yahoo.com>
2017-07-24 21:56:09 -0700

    more routes added

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,16 +4,16 @@ var path = require("path");
 module.exports = function(app) {
 
 	app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname + "/../public/home"));
+		res.sendFile(path.join(__dirname + "/../public/home.html"));
 	});
 
-	app.get("survey", function(req, res) {
+	app.get("/survey", function(req, res) {
 
-		res.sendFile(path.join(__dirname + "/../public/survey"));
+		res.sendFile(path.join(__dirname + "/../public/survey.html"));
 	});
 
 	app.use(function(req, res) {
-		res.sendFile(path.join(__dirname + "/../public/home"));
+		res.sendFile(path.join(__dirname + "/../public/home.html"));
 	});
 }
 
